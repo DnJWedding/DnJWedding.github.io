032c74e199bf64e7cbe2705e269264b76e891772
Halley Nathwani <aawajo@gmail.com>
2021-09-02 08:18:18 -0700

    placeholders for each page, starting to edit main page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,10 @@ function Footer() {
       <footer class="py-5 bg-dark fixed-bottom">
         <div class="container">
           <p class="m-0 text-center text-white">
-            Copyright &copy; Your Website 2020
+            PranamLLC 2021
+          </p>
+          <p class="m-0 text-center text-white" style={{'font-size':10 + 'px', 'font-style':'italic'}}>
+            Pranam to you.
           </p>
         </div>
       </footer>
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,48 +1,29 @@
 import React from "react";
+import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem, Nav, Navbar, NavDropdown, Container } from "react-bootstrap";
 import { Link, withRouter } from "react-router-dom";
 
 function Navigation(props) {
   return (
     <div className="navigation">
-      <nav class="navbar navbar-expand navbar-dark bg-dark">
-        <div class="container">
-          <Link class="navbar-brand" to="/">
-            React Multi-Page Website
-          </Link>
-          <div>
-            <ul class="navbar-nav ml-auto">
-              <li
-                class={`nav-item  ${
-                  props.location.pathname === "/" ? "active" : ""
-                }`}
-              >
-                <Link class="nav-link" to="/">
-                  Home
-                  <span class="sr-only">(current)</span>
-                </Link>
-              </li>
-              <li
-                class={`nav-item  ${
-                  props.location.pathname === "/about" ? "active" : ""
-                }`}
-              >
-                <Link class="nav-link" to="/about">
-                  About
-                </Link>
-              </li>
-              <li
-                class={`nav-item  ${
-                  props.location.pathname === "/contact" ? "active" : ""
-                }`}
-              >
-                <Link class="nav-link" to="/contact">
-                  Contact
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
+      <Navbar bg="dark" expand="lg" variant="dark">
+        <Container>
+          <Navbar.Brand href="/">Wedding</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav" class="navbar-nav ml-auto">
+            <Nav className="me-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/weddingagenda">Wedding Agenda</Nav.Link>
+              <NavDropdown id="nav-dropdown-dark-example" title="Dropdown">
+                  <NavDropdown.Item href="/aboutDhruv">About Dhruv</NavDropdown.Item>
+                  <NavDropdown.Item href="/historyOfNathwani">Nathwani History</NavDropdown.Item>
+                  <NavDropdown.Item href="/aboutDhruv">Beans</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="/aboutDhruv">More Beans</NavDropdown.Item>
+                </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </div>
   );
 }
