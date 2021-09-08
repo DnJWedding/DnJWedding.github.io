fda00fde93df866c23cdc1ea4b4f05f08eb79eaa
Halley Nathwani <aawajo@gmail.com>
2021-09-08 14:20:46 -0700

    fixing home page a bit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,36 +1,18 @@
 import React from "react";
 import Jumbotron from 'react-bootstrap/Jumbotron';
+import background from "./../images/tipNkiss.jpg"
 
 function Home() {
   return (
     <div className="home">
-      <div class="container">
+      <div class="container background"  style={{ backgroundImage: `url(${background})` }}>
         <div class="banner">
           <Jumbotron fluid>
-            <h1>Purshottam Manji Nathwani welcomes you to</h1>
-            <p>
-                the wedding of Jordan Buches and Dhruv Nathwani
-            </p>
+            <h1>Purshottam Manji Nathwani welcomes you</h1>
+            <p>to the wedding of Jordan Buches and Dhruv Nathwani</p>
           </Jumbotron>
         </div>
-        <div class="row align-items-center my-5">
-          <div class="col-lg-7">
-            <img
-              class="img-fluid rounded mb-4 mb-lg-0"
-              src="http://placehold.it/900x400"
-              alt=""
-            />
-          </div>
-          <div class="col-lg-5">
-            <h1 class="font-weight-light">Home</h1>
-            <p>
-              Lorem Ipsum is simply text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book.
-            </p>
-          </div>
-        </div>
+        
       </div>
     </div>
   );
