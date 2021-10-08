8529b11f6cabc810785da7de592e14ff35721f4b
JNathwani <12967322+Solaris333@users.noreply.github.com>
2021-10-08 18:40:38 -0400

    Fix Garbage

--- src/components/MusicSelection.jsx
+++ src/components/MusicSelection.jsx
@@ -14,62 +14,59 @@ function MusicSelection() {
           </div>
           <div className="col-lg-5">
             <h1 className="font-weight-light">The Music Selection</h1>
-              </h2>Ceremony</h2>
-                <ul>
-                  <li>Parental Seating: Across The Stars (Love Theme from Star Wars Episode 2: The Clone Wars) by John Williams</li>
-                    <p>
-                      The love theme that comes to us from a galaxy far, far away, <i>Across the Stars</i> was chosen for the parental seating 
-                      because the Bride and Groom really like Star Wars and love.
-                      </p>
-                  <li>Bridal Party Processional: The Ring Goes South (From Lord of the Rings: The Fellowship of the Ring) by Howard Shore</li>
-                    <p>
-                      The Bride and Groom chose <i>The Ring Goes South</i>, the theme of the Fellowship because it symbolized the journey
-                      many of the bridal party has come on with the bride and groom.
-                    </p>
-                  <li>Bridal Processional: A Thousand Years by The Piano Guys</li>
-                    <p>
-                    The Bride chose this version of A Thousand Years becuase The Piano Guys were one of two instrumental groups she and the 
-                    groom enjoy listening to (The other being Two Cellos).
-                    </p>
-                  <li>Bride and Groom Recessional: The Throne Room/End Theme (From Star Wars: A New Hope) By John Williams</li>
-                    <p>
-                    Another song from a galaxy far, far away, The Bride and Groom chose this song for their recessional because it 
-                    sybolizes unity and the victory over fate and foe.
-                    </p>
-              </h2>Reception</h2>
-                <ul>
-                  <li>Bride and Groom Introduction: Danger Zone by Kenny Loggins</li>
-                    <p>
-                      In an introduction that will take your breath away, The Bride and Groom are each other's wingman as they walk out as a 
-                      married couple for the first time. Hopefully they'll tear themselves from the heaven in each other's eyes long enough to 
-                      play with the boys (and girls) at the wedding before they head off to destinations unknown.
-                    </p>
-                    <p>
-                        Note: The Groom also apologizes for his inability to leave the puns alone.
-                    </p>
-                  <li>Bride and Groom Dance: Take on Me (Acoustic) by A-ha</li>
-                    <p>
-                      Take on Me is a classic song that's quirk and eccentricity match the Bride and Groom Perfectly
-                      </p>
-                  <li>Father-Daughter Dance: Accross The Universe by The Beatles</li>
-                    <p>
-                      Is this my espresso machine? Wh-what is-h-how did you get my espresso machine? My dad once told me, laugh and the world laughs with you, 
-                      Cry, and I'll give you something to cry about you little bastard! Must go faster. This thing comes fully loaded. AM/FM radio, reclining 
-                      bucket seats, and... power windows.
-                    </p>
-                  <li>Mother-Son Dance: ????? </li>
-                    <p>
-                    A mystery song from childhood, the Groom picked a somewhat unorthodox song to dance with his mother to. However, a couple hints to the 
-                    song are that it is from a Shah Rukh Khan movie and the sound track was listened to a lot while the Groom was growing up. Guessing the 
-                    song may be a bit of a gamble though....
-                    </p>
-                    
-            <p>
-              Lorem Ipsum is simply text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry&apos;s standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book.
-            </p>
+            <h2>Ceremony</h2>
+            <ul>
+              <li>Parental Seating: Across The Stars (Love Theme from Star Wars Episode 2: The Clone Wars) by John Williams</li>
+                <p>
+                  The love theme that comes to us from a galaxy far, far away, <i>Across the Stars</i> was chosen for the parental seating 
+                  because the Bride and Groom really like Star Wars and love.
+                  </p>
+              <li>Bridal Party Processional: The Ring Goes South (From Lord of the Rings: The Fellowship of the Ring) by Howard Shore</li>
+                <p>
+                  The Bride and Groom chose <i>The Ring Goes South</i>, the theme of the Fellowship because it symbolized the journey
+                  many of the bridal party has come on with the bride and groom.
+                </p>
+              <li>Bridal Processional: A Thousand Years by The Piano Guys</li>
+                <p>
+                The Bride chose this version of A Thousand Years becuase The Piano Guys were one of two instrumental groups she and the 
+                groom enjoy listening to (The other being Two Cellos).
+                </p>
+              <li>Bride and Groom Recessional: The Throne Room/End Theme (From Star Wars: A New Hope) By John Williams</li>
+                <p>
+                Another song from a galaxy far, far away, The Bride and Groom chose this song for their recessional because it 
+                sybolizes unity and the victory over fate and foe.
+                </p>
+            </ul>
+          </div>
+          <div className="col-lg-12">
+            <h2>Reception</h2>
+            <ul>
+              <li>Bride and Groom Introduction: Danger Zone by Kenny Loggins</li>
+                <p>
+                  In an introduction that will take your breath away, The Bride and Groom are each other's wingman as they walk out as a 
+                  married couple for the first time. Hopefully they'll tear themselves from the heaven in each other's eyes long enough to 
+                  play with the boys (and girls) at the wedding before they head off to destinations unknown.
+                </p>
+                <p>
+                    Note: The Groom also apologizes for his inability to leave the puns alone.
+                </p>
+              <li>Bride and Groom Dance: Take on Me (Acoustic) by A-ha</li>
+                <p>
+                  Take on Me is a classic song that's quirk and eccentricity match the Bride and Groom Perfectly
+                  </p>
+              <li>Father-Daughter Dance: Accross The Universe by The Beatles</li>
+                <p>
+                  Is this my espresso machine? Wh-what is-h-how did you get my espresso machine? My dad once told me, laugh and the world laughs with you, 
+                  Cry, and I'll give you something to cry about you little bastard! Must go faster. This thing comes fully loaded. AM/FM radio, reclining 
+                  bucket seats, and... power windows.
+                </p>
+              <li>Mother-Son Dance: <i>~a surprise~</i> </li>
+                <p>
+                A mystery song from childhood, the Groom picked a somewhat unorthodox song to dance with his mother to. However, a couple hints to the 
+                song are that it is from a Shah Rukh Khan movie and the sound track was listened to a lot while the Groom was growing up. Guessing the 
+                song may be a bit of a gamble though....
+                </p>
+            </ul>
           </div>
         </div>
       </div>
